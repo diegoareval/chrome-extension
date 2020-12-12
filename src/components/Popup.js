555a53ed0b1aa805c8768b3761b2067524643d74
Diego <diego2000avelar@gmail.com>
2020-12-12 08:13:11 -0600

    setup de extension google chrome con webpack

---
+++ src/components/Popup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Popup() {
+    return (
+        <div style={styles.main}>
+            <h1>Diego Arevalo</h1>
+        </div>
+    )
+}
+const styles = {
+    main: {
+        width: '300px',
+        height: '600px'
+    }
+}
+export default Popup;
